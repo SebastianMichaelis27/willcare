f4252780a88a22e7eb6b1f8402ad2b2501bc9f74
SebastianMichaelis27 <50473249+SebastianMichaelis27@users.noreply.github.com>
2025-05-03 22:24:18 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iframe Auto Resize</title>
+    <style>
+        iframe {
+            width: 100%;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <iframe id="iframe" src="https://www.stt-su.ac.id/flyer"></iframe>
+
+    <script>
+        document.querySelector("iframe").addEventListener( "load", function(e) {
+        });
+    </script>
+</body>
+</html>
